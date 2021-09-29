9979cbeb247be587770a1d570169192a437fea11
Matheus Gabriel Wozniak Passos <mgw356@hotmail.com>
2021-09-29 14:09:15 -0300

    update  script sql DER

--- backend-spring/gen/src/main/resources/db/script-sql/db_geracao_60_mysql_create.sql
+++ backend-spring/gen/src/main/resources/db/script-sql/db_geracao_60_mysql_create.sql
@@ -21,15 +21,13 @@ CREATE TABLE `tb_theme` (
 	`id_theme` bigint NOT NULL AUTO_INCREMENT,
 	`title` varchar(20) NOT NULL,
 	`description` varchar(40) NOT NULL,
-	`fk_id_post` bigint NOT NULL,
+	`hashtag` varchar(10) NOT NULL,
 	PRIMARY KEY (`id_theme`)
 );
 
 ALTER TABLE `tb_post` ADD CONSTRAINT `tb_post_fk0` FOREIGN KEY (`fk_id_user`) REFERENCES `tb_user`(`id_user`);
 
 ALTER TABLE `tb_post` ADD CONSTRAINT `tb_post_fk1` FOREIGN KEY (`fk_id_theme`) REFERENCES `tb_theme`(`id_theme`);
-
-ALTER TABLE `tb_theme` ADD CONSTRAINT `tb_theme_fk0` FOREIGN KEY (`fk_id_post`) REFERENCES `tb_post`(`id_post`);
 
 
 
