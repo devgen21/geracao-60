dc38fed19976b9bff1d80b0e4702c25c51e0da39
Joice-Simao <89137612+Joice-Simao@users.noreply.github.com>
2021-10-04 15:32:34 -0300

    Update doc

--- backend-spring/gen/src/main/java/br/com/gen60plus/gen/GenApplication.java
+++ backend-spring/gen/src/main/java/br/com/gen60plus/gen/GenApplication.java
@@ -2,6 +2,13 @@ package br.com.gen60plus.gen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * @author Cassia, Felipe, Joice, Matheus, Paulo
+ * @version 0.0.1
+ * @apiNote A aplicação Gen 60 plus é uma Api Rest criada durante o bootcamp da Generation.
+ *
+ */
 
 @SpringBootApplication
 public class GenApplication {
@@ -9,5 +16,4 @@ public class GenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GenApplication.class, args);
 	}
-
 }
