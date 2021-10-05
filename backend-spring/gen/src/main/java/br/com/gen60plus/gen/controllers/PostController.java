c97194a516b84eaba512e876bb18ce9edc5d3594
paulogrillo <paulodigitalart@gmail.com>
2021-10-05 14:00:51 -0300

    Updated class PostController insert endpoint Post

--- backend-spring/gen/src/main/java/br/com/gen60plus/gen/controllers/PostController.java
+++ backend-spring/gen/src/main/java/br/com/gen60plus/gen/controllers/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,11 +49,16 @@ public class PostController {
 		public ResponseEntity<List<Post>> GetByDescriptionTheme(@PathVariable String description){
 			return ResponseEntity.ok(repository.findAllByDescriptionContainingIgnoreCase(description));
 		}
-
 	    
 	    @GetMapping("/author/{author}")
 		public ResponseEntity<List<Post>> GetByAuthorTheme(@PathVariable String author){
 			return ResponseEntity.ok(repository.findAllByAuthorContainingIgnoreCase(author));
+		}
+	    
+		
+		@PostMapping
+		public ResponseEntity<Post> post(@RequestBody Post post){
+			return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(post));
 		}
 	    
 	    @PutMapping
