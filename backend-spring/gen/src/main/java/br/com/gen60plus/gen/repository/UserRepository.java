c8ed66022446bef0b76e5537e05d01a80b1ca328
paulogrillo <paulodigitalart@gmail.com>
2021-10-05 13:59:11 -0300

    Updated class UserRepository annotation

--- backend-spring/gen/src/main/java/br/com/gen60plus/gen/repository/UserRepository.java
+++ backend-spring/gen/src/main/java/br/com/gen60plus/gen/repository/UserRepository.java
@@ -3,9 +3,11 @@ package br.com.gen60plus.gen.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.gen60plus.gen.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository < User,Long>{
 	public List<User> findAllByUsernameContainingIgnoreCase(String username);
 	public List<User> findAllByEmailContainingIgnoreCase(String email);
