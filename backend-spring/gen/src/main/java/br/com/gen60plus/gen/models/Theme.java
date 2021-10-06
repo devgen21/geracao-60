b53e47b58fda517064905c37f250ed0288177ec6
paulogrillo <paulodigitalart@gmail.com>
2021-10-06 15:48:29 -0300

    Updated class Theme JsonIgnoreProprieties

--- backend-spring/gen/src/main/java/br/com/gen60plus/gen/models/Theme.java
+++ backend-spring/gen/src/main/java/br/com/gen60plus/gen/models/Theme.java
@@ -5,17 +5,21 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+
+@JsonIgnoreProperties("hibernateLazyInitializer")
 @Entity
 @Table (name = "tb_theme")
 public class Theme {
@@ -38,12 +42,9 @@ public class Theme {
 	@Size(min = 2, max = 40, message = "Minimo 2 caracteres maximo 10")
 	@Column(name = "Hashtags")
 	private String hashtags;
-
-	@OneToMany(mappedBy = "theme", cascade = CascadeType.ALL)
-	@JsonIgnoreProperties("theme")
+	
+	@OneToMany(mappedBy = "theme", fetch = FetchType.LAZY)
 	private List<Post> post;
-	 
-
 
 	public long getId() {
 		return id;
