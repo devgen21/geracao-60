729f4da75d428d2543a76fb7aad0b1af1156c875
Joice-Simao <89137612+Joice-Simao@users.noreply.github.com>
2021-10-13 12:42:37 -0300

    Update class UserRepository

--- backend-spring/gen/src/main/java/br/com/gen60plus/gen/repository/UserRepository.java
+++ backend-spring/gen/src/main/java/br/com/gen60plus/gen/repository/UserRepository.java
@@ -1,14 +1,14 @@
 package br.com.gen60plus.gen.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import br.com.gen60plus.gen.models.User;
 
-@Repository
-public interface UserRepository extends JpaRepository < User,Long>{
-	public List<User> findAllByUsernameContainingIgnoreCase(String username);
-	public List<User> findAllByEmailContainingIgnoreCase(String email);
+
+public interface UserRepository extends JpaRepository <User,Long>{
+	 List<User> findAllByUsernameContainingIgnoreCase(String username);
+	 List<User> findAllByEmailContainingIgnoreCase(String email);
+	 Optional<User> findAllByUser(String user);
 }
