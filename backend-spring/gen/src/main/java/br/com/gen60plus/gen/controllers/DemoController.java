6230726c1c403b1447f70a189f8d0f8cf88b5e67
Paulo Grillo <paulodigitalart@gmail.com>
2021-10-18 18:26:51 -0300

    new service demo upgrade version

---
+++ backend-spring/gen/src/main/java/br/com/gen60plus/gen/controllers/DemoController.java
@@ -0,0 +1,18 @@
+package br.com.gen60plus.gen.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class DemoController {
+	
+	@GetMapping
+	public ResponseEntity<String> demo() {
+		return ResponseEntity.ok("Working...");
+	}	
+}
