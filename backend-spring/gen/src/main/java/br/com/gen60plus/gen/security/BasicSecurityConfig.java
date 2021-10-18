47d162165565d2c29213e707b9c66769d18563f4
Paulo Grillo <paulodigitalart@gmail.com>
2021-10-18 18:57:01 -0300

    merge into main to heroku

--- backend-spring/gen/src/main/java/br/com/gen60plus/gen/security/BasicSecurityConfig.java
+++ backend-spring/gen/src/main/java/br/com/gen60plus/gen/security/BasicSecurityConfig.java
@@ -40,6 +40,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/demo").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
 		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers(HttpMethod.OPTIONS).permitAll()
