4374bc434541db8fabd93d0ebbac7407083953a9
Cássia Lima <cassiadosreislima@gmail.com>
2021-10-18 12:33:54 -0300

    Update rename class UserResponse

---
+++ backend-spring/gen/src/main/java/br/com/gen60plus/gen/models/UserResponse.java
@@ -0,0 +1,18 @@
+package br.com.gen60plus.gen.models;
+
+public class UserResponse {
+	
+	private String message;
+	
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+
+}
