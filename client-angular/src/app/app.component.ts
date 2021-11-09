70f7ea897a99d891352b9b5c99107da60d787fcc
Joice-Simao <89137612+Joice-Simao@users.noreply.github.com>
2021-11-09 14:37:05 -0300

    Update app component

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gen60plus';
+
+  constructor(
+    public auth: AuthService
+  ){}
+
 }
