f98886f02267992f396f9854ecb3c34284979d07
Joice-Simao <89137612+Joice-Simao@users.noreply.github.com>
2021-11-12 15:22:52 -0300

    Update Routing task 11

--- client-angular/src/app/app-routing.module.ts
+++ client-angular/src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { FeedComponent } from './feed/feed.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { TemaComponent } from './tema/tema.component';
 
 const routes: Routes = [
 
   {path: '', component: InicioComponent},
-  {path: 'entrar', component: EntrarComponent},
-  {path: 'cadastrar', component: CadastrarComponent},
-  {path: '**', component: NotFoundComponent}
 
+  {path: 'entrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent},  
+  {path: 'feed', component: FeedComponent},
+  {path: 'tema', component: TemaComponent},
+  
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
