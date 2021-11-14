6d1b5c716e198e08a33e17a6bbf0b9b6e1776ce0
Cássia Lima <cassiadosreislima@gmail.com>
2021-11-14 20:03:48 -0300

    Gen60plus component from inicio screen

---
+++ client-angular/src/app/gen60plus/gen60plus.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-gen60plus',
+  templateUrl: './gen60plus.component.html',
+  styleUrls: ['./gen60plus.component.css']
+})
+export class Gen60plusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(){
+    window.scroll(0,0)
+  }
+
+ 
+}
